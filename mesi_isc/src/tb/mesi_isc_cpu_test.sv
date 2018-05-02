62cd64878bf7b303050fbb58c9a50c0bdab3b359
YUWEI LIU <yliu8@kamek.(none)>
2018-05-02 16:05:36 -0500

    add failed property

--- mesi_isc/src/tb/mesi_isc_cpu_test.sv
+++ mesi_isc/src/tb/mesi_isc_cpu_test.sv
@@ -16,15 +16,7 @@ module cpu_sva (
     input [3:0] tb_ins_addr_i
 );
 
-/*
-assert_implication cpu0_cache_state_1_M (clk, ~rst, cache_state_cpu0[1] ==`MESI_ISC_TB_CPU_MESI_M, ((cache_state_cpu1[1] ==`MESI_ISC_TB_CPU_MESI_I) && (cache_state_cpu2[1] ==`MESI_ISC_TB_CPU_MESI_I) && (cache_state_cpu3[1] ==`MESI_ISC_TB_CPU_MESI_I)));
-*/
-
-/*
-assert_implication cpu1_cache_state_1_M (clk, ~rst, cache_state_cpu1[1] ==`MESI_ISC_TB_CPU_MESI_M, ((cache_state_cpu0[1] ==`MESI_ISC_TB_CPU_MESI_I) && (cache_state_cpu2[1] ==`MESI_ISC_TB_CPU_MESI_I) && (cache_state_cpu3[1] ==`MESI_ISC_TB_CPU_MESI_I)));
-*/
-
-
+/********************* check M & E *********************/
 generate
 	for(genvar i = 0; i <= 9; i++) begin
 	assert_implication cpu0_cache_state_M (clk, ~rst, cache_state_cpu0[i] ==`MESI_ISC_TB_CPU_MESI_M, ((cache_state_cpu1[i] ==`MESI_ISC_TB_CPU_MESI_I) && (cache_state_cpu2[i] ==`MESI_ISC_TB_CPU_MESI_I) && (cache_state_cpu3[i] ==`MESI_ISC_TB_CPU_MESI_I)));
@@ -53,7 +45,7 @@ generate
 end
 endgenerate
  
-
+/********************* check S *********************/
 generate
 	for(genvar i = 0; i <= 9; i++) begin
 	assert_implication cpu3_cache_state_S (clk, ~rst, cache_state_cpu3[i] ==`MESI_ISC_TB_CPU_MESI_S, ((cache_state_cpu0[i] ==`MESI_ISC_TB_CPU_MESI_I || cache_state_cpu0[i] == `MESI_ISC_TB_CPU_MESI_S) && (cache_state_cpu1[i] ==`MESI_ISC_TB_CPU_MESI_S || cache_state_cpu1[i] == `MESI_ISC_TB_CPU_MESI_I) && (cache_state_cpu2[i] ==`MESI_ISC_TB_CPU_MESI_I || cache_state_cpu2[i] == `MESI_ISC_TB_CPU_MESI_S)));
@@ -79,6 +71,16 @@ generate
 end
 endgenerate
 
+/********************* failed write starve property *********************/
+
+generate
+	for(genvar i = 0; i < 9; i++) begin
+	write_starve: assert property (@(posedge clk) disable iff(rst) 
+		(tb_ins_i == `MESI_ISC_TB_INS_WR) && (tb_ins_addr_i == i) |=>  ##[0:500] cache_state_cpu0[i] == `MESI_ISC_TB_CPU_MESI_M);
+	end
+endgenerate
+
+/********************* coverage *********************/
 
 generate
     for(genvar i=0; i<=9; i++) begin
@@ -120,24 +122,6 @@ cover_nop: cover property (@(posedge clk) tb_ins_i==`MESI_ISC_TB_INS_NOP);
 cover_write: cover property (@(posedge clk) tb_ins_i==`MESI_ISC_TB_INS_WR);
 cover_read: cover property (@(posedge clk) tb_ins_i==`MESI_ISC_TB_INS_RD);
 
-
-//generate
-//    for(genvar i=0; i<=9; i++) begin
-//write_cache : assert property (@(posedge clk) disable iff(rst) (cache_state_cpu0[i]==`MESI_ISC_TB_CPU_MESI_I) && (tb_ins_i==`MESI_ISC_TB_INS_WR)  && (tb_ins_addr_i==i)
-//        |=> ##[0:2000] cache_state_cpu0[i]==`MESI_ISC_TB_CPU_MESI_M);
-//
-//end
-//endgenerate
-
-//generate
-//    for(genvar i=0; i<=9; i++) begin
-//read_cache : assert property (@(posedge clk) disable iff(rst) (cache_state_cpu0[i]==`MESI_ISC_TB_CPU_MESI_M || cache_state_cpu0[i]==`MESI_ISC_TB_CPU_MESI_E) && (tb_ins_i==`MESI_ISC_TB_INS_RD)
-//        && (tb_ins_addr_i==i) && (c_state==`MESI_ISC_TB_CPU_M_STATE_IDLE) && !wr_proc_wait_for_en
-//        && !rd_proc_wait_for_en && (m_state==`MESI_ISC_TB_CPU_M_STATE_IDLE) && m_state_c_state_priority
-//        |=> m_state==`MESI_ISC_TB_CPU_M_STATE_RD_CACHE);
-//end
-//endgenerate
-
 endmodule 
 
 module cpu_sva_wrapper;
@@ -149,13 +133,13 @@ cpu_sva inst1 (
 		.cache_state_cpu1(tb.mesi_isc_tb_cpu1.cache_state),
 		.cache_state_cpu2(tb.mesi_isc_tb_cpu2.cache_state),
 		.cache_state_cpu3(tb.mesi_isc_tb_cpu3.cache_state),
-        .c_state(tb.mesi_isc_tb_cpu0.c_state),
-        .m_state(tb.mesi_isc_tb_cpu0.m_state),
-        .m_state_c_state_priority(tb.mesi_isc_tb_cpu0.m_state_c_state_priority),
-        .wr_proc_wait_for_en(tb.mesi_isc_tb_cpu0.wr_proc_wait_for_en),
-        .rd_proc_wait_for_en(tb.mesi_isc_tb_cpu0.rd_proc_wait_for_en),
-        .tb_ins_i(tb.mesi_isc_tb_cpu0.tb_ins_i),
-        .tb_ins_addr_i(tb.mesi_isc_tb_cpu0.tb_ins_addr_i)
+        	.c_state(tb.mesi_isc_tb_cpu0.c_state),
+		.m_state(tb.mesi_isc_tb_cpu0.m_state),
+		.m_state_c_state_priority(tb.mesi_isc_tb_cpu0.m_state_c_state_priority),
+		.wr_proc_wait_for_en(tb.mesi_isc_tb_cpu0.wr_proc_wait_for_en),
+		.rd_proc_wait_for_en(tb.mesi_isc_tb_cpu0.rd_proc_wait_for_en),
+		.tb_ins_i(tb.mesi_isc_tb_cpu0.tb_ins_i),
+		.tb_ins_addr_i(tb.mesi_isc_tb_cpu0.tb_ins_addr_i)
 );
 
 endmodule
